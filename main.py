dcb955dea87111d3005b7d031335aa0a62e439de
brunocmi94 <bruno@cavalomarinhoinformatica.com.br>
2021-08-16 11:41:46 -0300

    implemented click_on_frame, drag_window and resize_window custom events for top_bar

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from qt_core import *
 
 from gui.windows.ui_main_window import *
+from video_processing import *
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,6 +15,11 @@ class MainWindow(QMainWindow):
         self.setWindowFlag(Qt.FramelessWindowHint)
         self.ui.btn_open_video.clicked.connect(self.open_video_file)
         self.ui.btn_open_frames_dir.clicked.connect(self.select_frames_dir)
+        self.ui.top_bar.mousePressEvent = self.click_on_frame
+        self.ui.top_bar.mouseMoveEvent = self.drag_window
+        self.ui.top_bar.mouseDoubleClickEvent = self.resize_window
+        self.ui.btn_extract.clicked.connect(extract_frames)
+        self.maximized = False
 
         self.show()
 
@@ -26,13 +32,27 @@ class MainWindow(QMainWindow):
                                                       QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks)
         self.ui.frames_directory.setText(frames_dir)
 
-    def mousePressEvent(self, event):
+    def click_on_frame(self, event):
         self.oldPos = event.globalPosition().toPoint()
 
-    def mouseMoveEvent(self, event):
-        delta = QPoint(event.globalPosition().toPoint() - self.oldPos)
-        self.move(self.x() + delta.x(), self.y() + delta.y())
-        self.oldPos = event.globalPosition().toPoint()
+    def drag_window(self, event):
+        if self.maximized:
+            cursor_pos = QCursor.pos()
+            window_half_width = self.minimumWidth() / 2
+            self.resize_window(self)
+            self.move(cursor_pos.x() - window_half_width, cursor_pos.y())
+        else:
+            delta = QPoint(event.globalPosition().toPoint() - self.oldPos)
+            self.move(self.x() + delta.x(), self.y() + delta.y())
+            self.oldPos = event.globalPosition().toPoint()
+
+    def resize_window(self, event):
+        if self.isMaximized():
+            self.showNormal()
+            self.maximized = False
+        else:
+            self.showMaximized()
+            self.maximized = True
 
 
 if __name__ == "__main__":
