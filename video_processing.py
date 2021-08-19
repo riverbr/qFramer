07cefb29f768ae38ae38814c26fd9dde98cbd6b3
brunocmi94 <bruno@cavalomarinhoinformatica.com.br>
2021-08-19 15:41:02 -0300

    change flag name to thread_stopped and default value to False

--- video_processing.py
+++ video_processing.py
@@ -13,6 +13,7 @@ class ExtractFrames(QObject):
         self.frames_dir = ""
         self.frame_count = 0
         self.count = 0
+        self.thread_stopped = False
 
     def load_video(self):
         self.frames_dir.replace("\\", "/")
@@ -24,8 +25,13 @@ class ExtractFrames(QObject):
         success, image = self.vidcap.read()
         self.count = 0
         while success:
+            if self.thread_stopped:
+                break
             frame_dir_name = f"{self.frames_dir}/frame%d.jpg"
             cv2.imwrite(frame_dir_name % self.count, image)
             success, image = self.vidcap.read()
             self.count += 1
         self.finished.emit()
+
+    def stop(self):
+        self.thread_stopped = True
